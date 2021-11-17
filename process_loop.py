50658bd06984317b3144d2fbc5e6ab8db98694ec
andyto1234 <toshuho@gmail.com>
2021-11-17 17:18:08 +0000

    process on MSSL

--- process_loop.py
+++ process_loop.py
@@ -1,2 +1,27 @@
 from asheis import asheis
+import glob
+from asheis import asheis
+files = glob.glob('/disk/solar17/st3/python/data_eis/*.data.h5')
 
+for file in files:
+    myfits = asheis(file)
+    
+    try:
+        myfits.get_intensity('fe_12_195')
+    except:
+        pass
+
+    try:
+        myfits.get_velocity('fe_12_195')
+    except:
+        pass
+    
+    try:
+        myfits.get_width('fe_12_195')
+    except:
+        pass
+    
+    try:
+        myfits.get_composition('SiS')
+    except:
+        pass
