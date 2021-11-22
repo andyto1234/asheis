872834dbe2c946aded816f3556c6897ec03d7e4e
andyto1234 <toshuho@gmail.com>
2021-11-22 15:13:27 +0000

    eisfetch

--- eis_fetch_bs4.py
+++ eis_fetch_bs4.py
@@ -6,7 +6,7 @@ import multiprocessing as mp
 
 
 year = 2020
-months = [1,2,3,4,5,6,7,8,9,10,11,12]
+months = [1,2,3,4,5,6]
 
 for month in months:
     num_days = calendar.monthrange(year, month)[1]
