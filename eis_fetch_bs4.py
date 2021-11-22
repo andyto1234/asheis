f19b41bd8143cc625114b6b6664816709523e23f
andyto1234 <toshuho@gmail.com>
2021-11-22 17:19:44 +0000

    eisfetch

--- eis_fetch_bs4.py
+++ eis_fetch_bs4.py
@@ -5,23 +5,23 @@ import eispac
 import multiprocessing as mp
 
 
-year = 2020
+years = [2017,2018,2019,2020]
 months = [1,2,3,4,5,6]
 
 
-
-for month in months:
-    num_days = calendar.monthrange(year, month)[1]
-    days=[day+1 for day in range(num_days)]
-    month = '{:0>2}'.format(month)
-    for day in days:
-        day = '{:0>2}'.format(day)
-        url = f'https://eis.nrl.navy.mil/level1/hdf5/{year}/{month}/{day}/'
-        scraper = cfscrape.create_scraper()
-        html = scraper.get(url).content
-        soup = bs4.BeautifulSoup(html, 'html.parser')
-        iframes = soup.find_all(text=True)
-        iframes = [i for i in iframes if 'data.h5' in i]
-        pool = mp.Pool(processes=5)
-        mylist = [str(x) for y,x in enumerate(iframes)]
-        pool.map(eispac.download.download_hdf5_data, mylist)
+for year in years:
+    for month in months:
+        num_days = calendar.monthrange(year, month)[1]
+        days=[day+1 for day in range(num_days)]
+        month = '{:0>2}'.format(month)
+        for day in days:
+            day = '{:0>2}'.format(day)
+            url = f'https://eis.nrl.navy.mil/level1/hdf5/{year}/{month}/{day}/'
+            scraper = cfscrape.create_scraper()
+            html = scraper.get(url).content
+            soup = bs4.BeautifulSoup(html, 'html.parser')
+            iframes = soup.find_all(text=True)
+            iframes = [i for i in iframes if 'data.h5' in i]
+            pool = mp.Pool(processes=5)
+            mylist = [str(x) for y,x in enumerate(iframes)]
+            pool.map(eispac.download.download_hdf5_data, mylist,local_top='/disk/solar9/st3/data_eis')
