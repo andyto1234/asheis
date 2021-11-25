3e4e72058a9311e515b622854aaef128f9fbdbe3
andyto1234 <toshuho@gmail.com>
2021-11-25 13:15:08 +0000

    download

--- process_loop.py
+++ process_loop.py
@@ -1,26 +1,30 @@
 from asheis import asheis
 import glob
-files = glob.glob('/disk/solar9/st3/data_eis/*.data.h5')
 
-for file in files:
-    myfits = asheis(file)
-    
-    try:
-        myfits.get_intensity('fe_12_195')
-    except:
-        pass
 
-    try:
-        myfits.get_velocity('fe_12_195')
-    except:
-        pass
+if __name__ == '__main__':
     
-    try:
-        myfits.get_width('fe_12_195')
-    except:
-        pass
-    
-    try:
-        myfits.get_composition('SiS')
-    except:
-        pass
+    files = glob.glob('/disk/solar9/st3/data_eis/*.data.h5')
+
+    for file in files:
+        myfits = asheis(file)
+        
+        try:
+            myfits.get_intensity('fe_12_195')
+        except:
+            pass
+
+        try:
+            myfits.get_velocity('fe_12_195')
+        except:
+            pass
+        
+        try:
+            myfits.get_width('fe_12_195')
+        except:
+            pass
+        
+        try:
+            myfits.get_composition('SiS')
+        except:
+            pass
