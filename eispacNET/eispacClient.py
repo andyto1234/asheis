b4fc933254233c82557f5d1e316c0d28725add99
andyto1234 <toshuho@gmail.com>
2022-03-17 14:04:11 +0000

    add fido fetch support

--- eispacNET/eispacClient.py
+++ eispacNET/eispacClient.py
@@ -9,7 +9,7 @@ __all__ = ['EISPACClient']
 
 class EISPACClient(GenericClient):
     """
-    Provides access to Level 1 .h5 Extremeultraviolet Imaging Spectrometer (EIS NRL) data.
+    Provides access to Level 1 .h5 Extreme ultraviolet Imaging Spectrometer (EIS NRL) data.
 
     To use this client you must request Level 1 .h5 header and data.
     It is hosted by `NRL <https://eis.nrl.navy.mil/level1/hdf5/>`__.
@@ -19,18 +19,18 @@ class EISPACClient(GenericClient):
 
     >>> from sunpy.net import Fido, attrs as a
     >>> results = Fido.search(a.Time("2016/1/1", "2016/1/2"),
-    ...                       a.Instrument('EIS'), a.Provider('NRL'), a.Source('Hinode'))  #doctest: +REMOTE_DATA
+    ...                       a.Instrument('EIS'), a.Provider('NRL'), a.Source('Hinode'), a.Level('1'))  #doctest: +REMOTE_DATA
     >>> results  #doctest: +REMOTE_DATA
     <sunpy.net.fido_factory.UnifiedResponse object at ...>
     Results from 1 Provider:
     <BLANKLINE>
-    2 Results from the EVEClient:
-    Source: https://lasp.colorado.edu/eve/data_access/evewebdata/quicklook/L0CS/SpWx/
+    2 Results from the EISPACClient:
+    Source: https://eis.nrl.navy.mil/level1/hdf5/
     <BLANKLINE>
            Start Time               End Time        Instrument ... Provider Level
     ----------------------- ----------------------- ---------- ... -------- -----
-    2016-01-01 00:00:00.000 2016-01-01 23:59:59.999        EVE ...     LASP     0
-    2016-01-02 00:00:00.000 2016-01-02 23:59:59.999        EVE ...     LASP     0
+    2016-01-01 00:00:00.000 2016-01-01 23:59:59.999        EIS ...      NRL     1
+    2016-01-02 00:00:00.000 2016-01-02 23:59:59.999        EIS ...      NRL     1
     <BLANKLINE>
     <BLANKLINE>
 
@@ -46,8 +46,8 @@ class EISPACClient(GenericClient):
     @classmethod
     def register_values(cls):
         from sunpy.net import attrs
-        adict = {attrs.Instrument: [('EIS', 'Extreme ultraviolet Variability Experiment, which is part of the NASA Solar Dynamics Observatory mission.')],
-                 attrs.Source: [('Hinode', 'The Solar Dynamics Observatory.')],
-                 attrs.Provider: [('NRL', 'The Laboratory for Atmospheric and Space Physics.')],
-                 attrs.Level: [('1', 'EVE: The specific EVE client can only return Level 0C data. Any other number will use the VSO Client.')]}
+        adict = {attrs.Instrument: [('EIS', 'Extreme ultraviolet Imaging Spectrometer, which is part of the Hinode mission.')],
+                 attrs.Source: [('Hinode', 'Hinode (Solar-B).')],
+                 attrs.Provider: [('NRL', 'Naval Research Laboratory.')],
+                 attrs.Level: [('1', 'EIS: The specific EIS client can only return Level 1 .h5 data used in EISPAC.')]}
         return adict
