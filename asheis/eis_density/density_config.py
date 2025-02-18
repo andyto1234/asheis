16c9a25d77ea68f61f673cd2c9eea52d30d23811
andyto1234 <toshuho@gmail.com>
2025-02-18 17:08:10 +0800

    added second fe_12 186.88 density diagnostic

--- asheis/eis_density/density_config.py
+++ asheis/eis_density/density_config.py
@@ -9,7 +9,7 @@ DENSITY_DIAGNOSTICS = {
         'vmax': 10,
         'description': 'Fe XIII 203_202'
     },
-    'fe_12': {
+    'fe_12_186': {
         'sav_file': 'density_ratios_fe_12_186_88_195_12_.sav',
         'nom_line': 'fe_12_186.88',
         'denom_line': 'fe_12_195.12',
@@ -17,7 +17,7 @@ DENSITY_DIAGNOSTICS = {
         'vmax': 10,
         'description': 'Fe XII 186_195'
     },
-    'fe_12_196': {
+    'fe_12': {
         'sav_file': 'density_ratios_fe_12_196_64_195_12_.sav',
         'nom_line': 'fe_12_196.64',
         'denom_line': 'fe_12_195.12',
